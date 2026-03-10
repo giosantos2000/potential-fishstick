c4e64576c3b5ce1b3a77eee313f3be9d5ebe2472
Giovana Moreira dos Santos <giovana.santosgms@gmail.com>
2026-03-10 18:26:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# 127.0.0.1:60290
-@app.get("/")
+@app.get("/helloword")
 async def root():
     return {"message": "Hello World"}
 
-#127.0.0.1:60290/teste1
 @app.get("/teste1")
 async def funcaoteste():
     return {"teste": "deu certo"}
